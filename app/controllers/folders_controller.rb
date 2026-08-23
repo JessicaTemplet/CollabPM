b6f7065318b345fb7469683a936306f3c9a99672
Jessica Templet <jessift82@gmail.com>
2026-08-23 04:39:21 -0500

    chore: suppress semgrep false positives in FoldersController

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -3,8 +3,13 @@ class FoldersController < ApplicationController
     @folder = Current.tenant.folders.new(folder_params)
 
     if @folder.save
+      # nosemgrep: ruby.rails.security.audit.xss.avoid-redirect.avoid-redirect
+      # Destination is documents_path; folder_id is an internal record id,
+      # not an arbitrary redirect target.
       redirect_to documents_path(folder_id: @folder.parent_id), notice: "Folder created."
     else
+      # nosemgrep: ruby.rails.security.audit.xss.avoid-redirect.avoid-redirect
+      # Same destination as above; only the alert text is dynamic.
       redirect_to documents_path(folder_id: @folder.parent_id), alert: @folder.errors.full_messages.to_sentence
     end
   end
